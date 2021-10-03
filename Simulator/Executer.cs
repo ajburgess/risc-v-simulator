acd4c84cb5728ef035bfc59bde401ae8002d0c46
Andrew Burgess <developerandrewburgess@outlook.com>
2021-10-03 09:04:31 +0100

    refactor

--- Simulator/Executer.cs
+++ Simulator/Executer.cs
@@ -7,68 +7,73 @@ namespace Simulator
         public static void Execute(DecodeInfo info, RegisterSet registers, WordMemory memory, ref UInt32 pc)
         {
             UInt32 nextPC = pc + 4;
+            UInt32 rs1_value = registers[info.RS1];
+            UInt32 rs2_value = registers[info.RS2];
+            Byte reg_shift = (Byte)(rs2_value & 0x1F);
+            Byte imm_shift = (Byte)(info.I_Immediate & 0x1F);
+
             switch (info.Instruction)
             {
                 case Instruction.ADD:
-                    registers[info.RD] = registers[info.RS1] + registers[info.RS2];
+                    registers[info.RD] = rs1_value + rs2_value;
                     break;
                 case Instruction.SUB:
-                    registers[info.RD] = registers[info.RS1] - registers[info.RS2];
+                    registers[info.RD] = rs1_value - rs2_value;
                     break;
                 case Instruction.SLL:
-                    registers[info.RD] = registers[info.RS1] << ((Byte)registers[info.RS2] & 0x1F);
+                    registers[info.RD] = rs1_value << reg_shift;
                     break;
                 case Instruction.SRL:
-                    registers[info.RD] = registers[info.RS1] >> (Byte)(registers[info.RS2] & 0x1F);
+                    registers[info.RD] = rs1_value >> reg_shift;
                     break;
                 case Instruction.SRA:
-                    registers[info.RD] = Logic.ShiftRightArithmetic(registers[info.RS1], (Byte)(registers[info.RS2] & 0x1F));
+                    registers[info.RD] = Logic.ShiftRightArithmetic(rs1_value, reg_shift);
                     break;
                 case Instruction.OR:
-                    registers[info.RD] = registers[info.RS1] | registers[info.RS2];
+                    registers[info.RD] = rs1_value | rs2_value;
                     break;
                 case Instruction.AND:
-                    registers[info.RD] = registers[info.RS1] & registers[info.RS2];
+                    registers[info.RD] = rs1_value & rs2_value;
                     break;
                 case Instruction.XOR:
-                    registers[info.RD] = registers[info.RS1] ^ registers[info.RS2];
+                    registers[info.RD] = rs1_value ^ rs2_value;
                     break;
                 case Instruction.SLTU:
-                    registers[info.RD] = Logic.UnsignedLessThan(registers[info.RS1], registers[info.RS2]) ? 1u : 0;
+                    registers[info.RD] = Logic.UnsignedLessThan(rs1_value, rs2_value) ? 1u : 0;
                     break;
                 case Instruction.SLT:
-                    registers[info.RD] = Logic.SignedLessThan(registers[info.RS1], registers[info.RS2]) ? 1u : 0;
+                    registers[info.RD] = Logic.SignedLessThan(rs1_value, rs2_value) ? 1u : 0;
                     break;
                 case Instruction.ADDI:
-                    registers[info.RD] = registers[info.RS1] + info.I_Immediate;
+                    registers[info.RD] = rs1_value + info.I_Immediate;
                     break;
                 case Instruction.ANDI:
-                    registers[info.RD] = registers[info.RS1] & info.I_Immediate;
+                    registers[info.RD] = rs1_value & info.I_Immediate;
                     break;
                 case Instruction.ORI:
-                    registers[info.RD] = registers[info.RS1] | info.I_Immediate;
+                    registers[info.RD] = rs1_value | info.I_Immediate;
                     break;
                 case Instruction.XORI:
-                    registers[info.RD] = registers[info.RS1] ^ info.I_Immediate;
+                    registers[info.RD] = rs1_value ^ info.I_Immediate;
                     break;
                  case Instruction.SLLI:
-                    registers[info.RD] = registers[info.RS1] << (Byte)(info.I_Immediate & 0x1F);
+                    registers[info.RD] = rs1_value << imm_shift;
                     break;
                  case Instruction.SRLI:
-                    registers[info.RD] = registers[info.RS1] >> (Byte)(info.I_Immediate & 0x1F);
+                    registers[info.RD] = rs1_value >> imm_shift;
                     break;
                  case Instruction.SRAI:
-                    registers[info.RD] = Logic.ShiftRightArithmetic(registers[info.RS1], (Byte)(info.I_Immediate & 0x1F));
+                    registers[info.RD] = Logic.ShiftRightArithmetic(rs1_value, imm_shift);
                     break;
                 case Instruction.SLTIU:
-                    registers[info.RD] = Logic.UnsignedLessThan(registers[info.RS1], info.I_Immediate) ? 1u : 0;
+                    registers[info.RD] = Logic.UnsignedLessThan(rs1_value, info.I_Immediate) ? 1u : 0;
                     break;
                 case Instruction.SLTI:
-                    registers[info.RD] = Logic.SignedLessThan(registers[info.RS1], info.I_Immediate) ? 1u : 0;
+                    registers[info.RD] = Logic.SignedLessThan(rs1_value, info.I_Immediate) ? 1u : 0;
                     break;
                 case Instruction.LW:
                 {
-                    UInt32 address = registers[info.RS1] + info.I_Immediate;
+                    UInt32 address = rs1_value + info.I_Immediate;
                     UInt32 wordLitteEndian = memory[address >> 2];
                     UInt32 wordBigEndian = Logic.ReverseEndian(wordLitteEndian);
                     registers[info.RD] = wordBigEndian;
@@ -76,7 +81,7 @@ namespace Simulator
                 }
                 case Instruction.LBU:
                 {
-                    UInt32 address = registers[info.RS1] + info.I_Immediate;
+                    UInt32 address = rs1_value + info.I_Immediate;
                     UInt32 word = memory[address >> 2];
                     Byte byteOffset = (Byte)(3 - (address & 0x03));
                     Byte bitsOffset = (Byte)(byteOffset << 3);
@@ -86,7 +91,7 @@ namespace Simulator
                 }
                 case Instruction.LB:
                 {
-                    UInt32 address = registers[info.RS1] + info.I_Immediate;
+                    UInt32 address = rs1_value + info.I_Immediate;
                     UInt32 word = memory[address >> 2];
                     Byte byteOffset = (Byte)(3 - (address & 0x03));
                     Byte bitsOffset = (Byte)(byteOffset << 3);
@@ -97,7 +102,7 @@ namespace Simulator
                 }
                 case Instruction.LHU:
                 {
-                    UInt32 address = (UInt32)(registers[info.RS1] + info.I_Immediate);
+                    UInt32 address = (UInt32)(rs1_value + info.I_Immediate);
                     UInt32 word = memory[address >> 2];
                     Byte halfOffset = (Byte)(2 - (address & 0x02));
                     Byte bitsOffset = (Byte)(halfOffset << 3);
@@ -108,7 +113,7 @@ namespace Simulator
                 }
                 case Instruction.LH:
                 {
-                    UInt32 address = (UInt32)(registers[info.RS1] + info.I_Immediate);
+                    UInt32 address = (UInt32)(rs1_value + info.I_Immediate);
                     UInt32 word = memory[address >> 2];
                     UInt16 halfOffset = (UInt16)(2 - (address & 0x02));
                     UInt16 bitsOffset = (UInt16)(halfOffset * 8);
